e4039156a021b163050bd19144854939ec401374
gyeonghyeonkwon <kyanghyang12@naver.com>
2024-11-18 17:20:48 +0900

    member setting update

--- src/main/java/com/ll/blog/domain/member/service/MemberServiceImpl.java
+++ src/main/java/com/ll/blog/domain/member/service/MemberServiceImpl.java
@@ -1,12 +1,13 @@
 package com.ll.blog.domain.member.service;
 
 import com.ll.blog.domain.member.repository.MemberRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository;
