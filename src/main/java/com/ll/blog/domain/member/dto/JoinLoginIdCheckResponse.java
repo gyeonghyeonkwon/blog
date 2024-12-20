f9fa2a49b5ff1bc2c39b907139e69c38c22c1a9a
gyeonghyeonkwon <kyanghyang12@naver.com>
2024-12-20 20:47:23 +0900

    JoinLoginIdCheckResponse record 변경

--- src/main/java/com/ll/blog/domain/member/dto/JoinLoginIdCheckResponse.java
+++ src/main/java/com/ll/blog/domain/member/dto/JoinLoginIdCheckResponse.java
@@ -1,14 +1,6 @@
 package com.ll.blog.domain.member.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
-public class JoinLoginIdCheckResponse {
+public record JoinLoginIdCheckResponse(String loginId, boolean availability) {
 
-  private String loginId; //사용하고 싶은 요청아이디
-  private boolean availability; //사용 가능여부 false 면 사용가능
 }
